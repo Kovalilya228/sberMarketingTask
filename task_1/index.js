ce0e5e57872b4e625dea9ea6ccdb8326e8ea09ab
Ilya Koval <ilya.koval.2002@yandex.ru>
2024-04-26 00:49:41 +0300

    first attempt to make task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,48 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const exceptions = {
+    'groupId' : true,
+    'service' : true,
+    'formatSize' : true,
+    'ca' : true
+};
+const countOfMoves = initializeCountOfMoves(translations);
 
+console.log("Let's rock");
+console.log(encoded, translations);
 
+const decoded = encoded.map(item => decode(item, translations));
+const uniqIds = checkUniq(countOfMoves);
 
-// console.log(decoded)
+console.log(decoded);
+console.log(uniqIds);
+
+function decode(object, translations) {
+    const decodedObject = {};
+    Object.assign(decodedObject, object);
+    for (let key in object) {
+        if (key.toLowerCase().includes('id') && !exceptions[key] && decodedObject[key] in translations) {
+            countOfMoves[decodedObject[key]] += 1;
+            decodedObject[key] = translations[decodedObject[key]];
+        }
+    }
+    return decodedObject
+}
+
+function initializeCountOfMoves(source) {
+    const res = {};
+    for (let key in source) {
+        res[key] = 0;
+    }
+    return res;
+}
+
+function checkUniq(countOfMoves) {
+    const res = [];
+    for (let key in countOfMoves) {
+        if (countOfMoves[key] === 1) {
+            res.push(key);
+        }
+    }
+    return res;
+}
